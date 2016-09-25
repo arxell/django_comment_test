a3601b53beeefda2ff28eb7e945e1e53ae942308
Anton Ogorodnikov <anton.ogorodnikov@ostrovok.ru>
2016-09-25 16:30:45 +0300

    import data improves

--- app/comment/management/commands/import_data.py
+++ app/comment/management/commands/import_data.py
@@ -16,22 +16,22 @@ def random_string(size=6, chars=string.ascii_uppercase + string.digits):
 USER_IDS = list(range(1, 100))
 OBJECT_IDS = list(range(1, 1000))
 
-COMMENTS_FOR_OBJECTS_NUMBER = 100
-COMMENTS_FOR_COMMENTS_NUMBER = 1000000
+COMMENTS_FOR_OBJECTS_NUMBER = 1000
+COMMENTS_FOR_COMMENTS_NUMBER = 100000
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        # for i in range(0, COMMENTS_FOR_OBJECTS_NUMBER):
-        #     Comment.objects.create(
-        #         object_id=random.choice(OBJECT_IDS),
-        #         parent=None,
-        #         text=random_string(),
-        #         type_id=random.choice(settings.COMMENT_TYPES_LIST),
-        #         user_id=random.choice(USER_IDS)
-        #     )
+        for i in range(0, COMMENTS_FOR_OBJECTS_NUMBER):
+            Comment.objects.create(
+                object_id=random.choice(OBJECT_IDS),
+                parent=None,
+                text=random_string(),
+                type_id=random.choice(settings.COMMENT_TYPES_LIST),
+                user_id=random.choice(USER_IDS)
+            )
 
         parents_ids = list(Comment.objects.all().values_list('id', flat=True))
         for i in range(0, COMMENTS_FOR_COMMENTS_NUMBER):
